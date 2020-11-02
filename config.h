faa43cf8bf3fece3282f71b8bbc998fcfcacef75
Clarence Cheong <clarence_han[at]hotmail[dot]com>
2020-11-02 10:12:31 +0800

    feat: pavucontrol now floating

--- config.h
+++ config.h
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
 	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 	{ "xfce4-terminal",  NULL,   NULL,       0,            0,           -1 },
+	{ "pavucontrol",     NULL,   NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
