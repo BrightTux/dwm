58a82bbb65d0a2054ba62d427506b3d1b33b12b8
Clarence Cheong <clarence_han[at]hotmail[dot]com>
2021-09-02 10:27:10 +0800

    feat: rofi-pass

--- config.h
+++ config.h
@@ -69,6 +69,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", "--opacity", "85" };
+static const char *rofipasscmd[] = {"rofi-pass", NULL};
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
@@ -81,7 +82,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_y,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_y,      spawn,          {.v = rofipasscmd } },
 
   { 0,                            XF86XK_AudioLowerVolume, 	spawn, 		{.v = downvol } },
 	{ 0,                       	    XF86XK_AudioMute, 	    	spawn, 		{.v = mutevol } },
